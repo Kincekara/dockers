dab58c43ea9444570a02f41634d82de69973dcb5
Kincekara <kutluhanincekara@hotmail.com>
2023-04-12 15:31:19 +0000

    code clean up

--- cbird-util/alpine/0.8/report_gen.py
+++ cbird-util/alpine/0.8/report_gen.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
 """
 Report generating script for C-BIRD workflow
-Created on Thu Aug  4 13:43:46 2022
-
+2023-04-12
 @author: Kutluhan Incekara
 email: kutluhan.incekara@ct.gov
 """
@@ -11,200 +9,344 @@ import argparse
 import pandas as pd
 from datetime import date
 
-# report_generator.py \
-# ~{samplename} \
-# ~{taxon_report} \
-# ~{mlst_report} \
-# ~{amr_report} \
-# ~{plasmid_report} \
-# ~{version}
+# Define functions
+def read_bracken_report(taxon_report):
+    # get bracken report
+    df = pd.read_csv(taxon_report, sep = '\t')
+    df = df.sort_values(by=['fraction_total_reads'], ascending=False)
 
-def main():
+    # find top tax id & write
+    with open("taxid.txt", "w") as f:
+        taxid = df["taxonomy_id"][0]
+        f.write(str(taxid))
+
+    # Species
+    df2 = df[["name", "fraction_total_reads" ]].copy()
+    df2['fraction_total_reads'] = df2['fraction_total_reads'].apply(lambda x: round(x*100, 2))
+    df2 = df2.rename(columns={"name": "Microorganism", "fraction_total_reads": "Abundance(%)"})
+    return df2
+
+def read_mlst_report(mlst_report):
+    df = pd.read_csv(mlst_report, sep="\t", skiprows=1, header=None)
+    df = df.drop(columns=0)
+    columns = ['Scheme','Type (ST)','Alleles']
+    if len(df.columns) < 3:
+        df = pd.DataFrame(columns=columns)
+    else:
+        for i in range(3,len(df.columns)):
+            columns.append('')
+        df.columns = columns    
+    return df
+
+def read_amr_report(amr_report):
+    df =  pd.read_csv(amr_report, sep = '\t')    
+    df2 = df[['Gene symbol','Sequence name','Class','Subclass','% Coverage of reference sequence','% Identity to reference sequence', 'Element type']]
+    df2 = df2.rename(columns={'Gene symbol':'Gene','Sequence name':'Description','Class':'AR Class',
+                                    'Subclass':'AR Subclass','% Coverage of reference sequence':'Coverage(%)',
+                                    '% Identity to reference sequence':'Identity(%)'})
+
+    amr_df = df2.loc[df2['Element type'] == "AMR"]
+    amr_df = amr_df.drop(columns={'Element type'})
+    amr_df = amr_df.sort_values(by=['AR Class','AR Subclass'], ascending=True)
+
+    stress_df = df2.loc[df2['Element type'] == "STRESS"]
+    stress_df = stress_df.drop(columns={'Element type'})
+    stress_df = stress_df.sort_values(by=['AR Class','AR Subclass'], ascending=True)
+
+    vir_df = df2.loc[df2['Element type'] == "VIRULENCE"]
+    vir_df = vir_df.drop(columns={'Element type','AR Class','AR Subclass'})
+
+    df_list = [amr_df, stress_df, vir_df]
+    return df_list
+
+def read_plasmidfinder_report(plasmid_report):
+    df = pd.read_csv(plasmid_report, sep = '\t')
+    df2 = df[['Plasmid','Identity','Query / Template length', 'Accession number']].copy()
+    df2 = df2.sort_values(by=['Identity','Plasmid'], ascending=False)
+    df2 = df2.rename(columns={'Identity':'Identity(%)'})
+    return df2 
+
+def read_mash_report(mash_report):
+    df = pd.read_csv(mash_report, sep = '\t', header=None)
+    df.columns = ['Microorganism', 'Identity(%)']
+    return df
+
+def read_blast_report(blast_report):
+    df = pd.read_csv(blast_report, sep = '\t')
+    if not df.empty:
+        df['Coverage(%)'] = abs((df['send'] - df['sstart'] +1)//3) / (df['qend'] - df['qstart'] +1 )*100
+        df.drop(columns=['sseqid', 'mismatch','gapopen','qstart','qend','sstart','send','evalue','bitscore'], inplace=True)
+        df.rename(columns={'qseqid':'Gene','pident':'Identity(%)','length':'Length'}, inplace=True)
+        df=df[['Gene','Length','Coverage(%)','Identity(%)']]
+    return df
+
+def clia_report(samplename, mash, amr, version, footer):
+    # Enterobacter cloacae complex
+    ecc = ["Enterobacter cloacae complex sp.", "Enterobacter cloacae", "Enterobacter hormaechei",
+    "Enterobacter asburiae", "Enterobacter cancerogenus", "Enterobacter chengduensis",
+    "Enterobacter kobei", "Enterobacter ludwigii", "Enterobacter roggenkampii", "Enterobacter sichuanensis"]
+    mash.loc[mash['Microorganism'].isin(ecc), 'Microorganism'] = "Enterobacter cloacae complex"
+    # Klebsiella oxytoca complex
+    koc = ["Klebsiella oxytoca", "Klebsiella michiganensis", "Klebsiella grimontii", "Klebsiella huaxiensis",
+    "Klebsiella huaxiensis", "Klebsiella pasteurii", "Klebsiella spallanzanii"]
+    mash.loc[mash['Microorganism'].isin(koc), 'Microorganism'] = "Klebsiella oxytoca complex"
+    
+    # report
+    page_title = samplename
+    title = "Report"
+    stitle1 = "Sample Information"
+    stitle2 = "Predicted Organism"
+    stitle3 = "Resistance Genes"
+    today = date.today()
+    # HTML #
+    html = f'''
+    <!DOCTYPE html>
+    <html lang="en-us">
+        <head>
+            <meta charset="UTF-8">
+            <title>{page_title}</title>
+            <style>
+            h1 {{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1.5em;
+            text-align: center;
+            color: #1e5c85
+            }}
+
+            h2 {{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1em;
+            margin-top: 1em;
+            margin-bottom: 0.4em;
+            text-align: left;
+            color: #D6672E 
+            }}
+
+            table {{
+            border-collapse: collapse;
+            border: none;
+            }}
+            
+            th, td {{
+            text-align: left;
+            padding-left: 5px;
+            padding-right: 5px;
+            padding-top: 1px;
+            padding-bottom: 1px;
+            border: none;
+            }}
+            
+            table.dataframe tr:nth-child(even) {{background-color: #f2f2f2;}}
+            
+            table.dataframe th {{
+            background-color: #1e5c85;
+            color: white;
+            
+            }}
+            footer {{
+                    font-size: 0.8em
+                }}      
+            </style>
+        </head>
+        <body>
+        <header>
+        <h1>{title}</h1>
+        </header>
+        <hr>
+        <article>
+        <h2>{stitle1}</h2>
+        <table>
+        <tr>
+        <td>Report date:</td>
+        <td>{today}</td>
+        </tr>
+        <tr>
+        <td>Laboratory ID:</td>
+        <td>{samplename}</td>
+        </tr>
+        </table>
+        <h2>{stitle2}</h2>
+        {mash.to_html(index=False, justify="left")}
+        <h2>{stitle3}</h2>
+        {amr.to_html(index=False, justify="left")}
+        <p></p>
+        </article>
+        <hr>
+        <footer>
+        <p><i>This report is created by <a href="https://github.com/Kincekara/C-BIRD">{version}</a> bioinformatics pipeline.</br>
+        {footer}</i></p>
+        </footer>
+    </body>
+    </html>
+    '''
+    # write html file
+    with open(samplename + "_clia_report.html", "w") as f:
+        f.write(html)
+    print(samplename + "_clia_report.html is created!")
+
+def epi_report(samplename, bracken, mlst, amr, stress, virulence, plasmid, blast, mash, version):
+    page_title = samplename
+    title = "Report"
+    stitle1 = "Sample Information"
+    stitle2 = "Taxonomic Profile of Reads"
+    stitle3 = "Predicted Organism"
+    stitle4 = "MLST Typing"
+    stitle5 = "Resistance Genes"
+    stitle6 = "Stress Genes"
+    stitle7 = "Hypervirulance Genes"
+    stitle8 = "Target Protein Search"
+    stitle9 = "Plasmids Detected"    
+    today = date.today()
+    footer = "Research use only."
+    
+    # HTML #
+    html = f'''
+    <!DOCTYPE html>
+    <html lang="en-us">
+        <head>
+            <meta charset="UTF-8">
+            <title>{page_title}</title>
+            <style>
+            h1 {{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1.5em;
+            text-align: center;
+            color: #1e5c85
+            }}
+
+            h2 {{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1em;
+            margin-top: 1em;
+            margin-bottom: 0.4em;
+            text-align: left;
+            color: #D6672E 
+            }}
+
+            table {{
+            border-collapse: collapse;
+            border: none;
+            }}
+            
+            th, td {{
+            text-align: left;
+            padding-left: 5px;
+            padding-right: 5px;
+            padding-top: 1px;
+            padding-bottom: 1px;
+            border: none;
+            }}
+            
+            table.dataframe tr:nth-child(even) {{background-color: #f2f2f2;}}
+            
+            table.dataframe th {{
+            background-color: #1e5c85;
+            color: white;
+            
+            }}
+            footer {{
+                    font-size: 0.8em
+                }}      
+            </style>
+        </head>
+        <body>
+        <header>
+        <h1>{title}</h1>
+        </header>
+        <hr>
+        <article>
+        <h2>{stitle1}</h2>
+        <table>
+        <tr>
+        <td>Report date:</td>
+        <td>{today}</td>
+        </tr>
+        <tr>
+        <td>Laboratory ID:</td>
+        <td>{samplename}</td>
+        </tr>
+        </table>
+        <h2>{stitle2}</h2>
+        {bracken.to_html(index=False, justify="left")}
+        <h2>{stitle3}</h2>
+        {mash.to_html(index=False, justify="left")}
+        <h2>{stitle4}</h2>
+        {mlst.to_html(index=False, justify="left")}
+        <h2>{stitle5}</h2>
+        {amr.to_html(index=False, justify="left")}
+        <h2>{stitle6}</h2>
+        {stress.to_html(index=False, justify="left")}
+        <h2>{stitle7}</h2>
+        {virulence.to_html(index=False, justify="left")}
+        <h2>{stitle8}</h2>
+        {blast.to_html(index=False, justify="left")}
+        <h2>{stitle9}</h2>
+        {plasmid.to_html(index=False, justify="left")}
+        <p></p>
+        </article>
+        <hr>
+        <footer>
+        <p><i>This report is created by <a href="https://github.com/Kincekara/C-BIRD">{version}</a> bioinformatics pipeline.</br>
+        {footer}</i></p>
+        </footer>
+    </body>
+    </html>
+    '''
+    # write html file
+    with open(samplename + "_html_report.html", "w") as f:
+        f.write(html)     
+    print(samplename + "_html_report.html is created!")
+
+# Main #
+def main(args):
     samplename = args.samplename
     taxon_report = args.taxon_report
     mlst_report = args.mlst_report
     amr_report = args.amr_report
     plasmid_report = args.plasmid_report
-    version = args.version
-    footer_note = args.footer_note
+    mash_report = args.mash_report
+    version = args.cbird_version
+    footer = args.footer_note
+    blast_report = args.blast_report
 
-    ## Bracken ##
-    # get bracken report
-    b_df = pd.read_csv(taxon_report, sep = '\t')
-    b_df = b_df.sort_values(by=['fraction_total_reads'], ascending=False)
-
-    # find top tax id & write
-    with open("taxid.txt", "w") as f:
-        taxid = b_df["taxonomy_id"][0]
-        f.write(str(taxid))
-
-    # Species
-    b_df2 = b_df[["name", "fraction_total_reads" ]].copy()
-    b_df2['fraction_total_reads'] = b_df2['fraction_total_reads'].apply(lambda x: round(x*100, 2))
-    b_df2 = b_df2.rename(columns={"name": "Microorganism", "fraction_total_reads": "Abundance(%)"})
-
-    ## MLST##
-    mlst_df = pd.read_csv(mlst_report, sep="\t", skiprows=1, header=None)
-    mlst_df = mlst_df.drop(columns=0)
-    columns = ['Scheme','Type (ST)','Alleles']
-    if len(mlst_df.columns) < 3:
-        mlst_df = pd.DataFrame(columns=columns)
+    # read reports
+    bracken = read_bracken_report(taxon_report)
+    mlst = read_mlst_report(mlst_report)
+    amr = read_amr_report(amr_report)[0]
+    stress = read_amr_report(amr_report)[1]
+    virulence = read_amr_report(amr_report)[2]    
+    plasmid = read_plasmidfinder_report(plasmid_report)
+    if blast_report is not None:
+        blast = read_blast_report(blast_report)
     else:
-        for i in range(3,len(mlst_df.columns)):
-            columns.append('')
-        mlst_df.columns = columns
-
-    ## AMR ##  
-    r_df =  pd.read_csv(amr_report, sep = '\t')
-    amr_df = r_df.loc[r_df['Element type'] == "AMR"]
-    amr_df2 = amr_df[['Gene symbol','Sequence name','Class','Subclass','% Coverage of reference sequence','% Identity to reference sequence']]
-    amr_df2 = amr_df2.rename(columns={'Gene symbol':'Gene','Sequence name':'Description','Class':'AR Class',
-                                      'Subclass':'AR Subclass','% Coverage of reference sequence':'Coverage(%)',
-                                      '% Identity to reference sequence':'Identity(%)'})
-    amr_df2 = amr_df2.sort_values(by=['AR Class','AR Subclass'], ascending=True)
-
-    ## Plasmid ##
-    p_df = pd.read_csv(plasmid_report, sep = '\t')
-    p_df2 = p_df[['Plasmid','Identity','Query / Template length', 'Accession number']].copy()
-    p_df2 = p_df2.sort_values(by=['Identity','Plasmid'], ascending=False)
-    p_df2 = p_df2.rename(columns={'Identity':'Identity(%)'})
-
-
-    ## Report template ##
-    page_title = samplename
-    title = "Report"
-    stitle1 = "Sample Information"
-    stitle2 = "Taxonomic Profile of Reads"
-    stitle3 = "MLST Typing"
-    stitle4 = "Resistance"
-    stitle5 = "Plasmids Detected"
-    today = date.today()
+        # create empty dataframe
+        blast = pd.DataFrame(columns=['Gene','Length','Identity(%)', 'Coverage(%)'])
     
-    
-    if mlst_df.empty:
-        mlst_txt = "No MLST scheme is found!"
+    if mash_report is not None:
+        mash = read_mash_report(mash_report)
+        # write clia report
+        clia_report(samplename, mash, amr, version, footer)
     else:
-        mlst_txt = mlst_df.to_string(index=False)
-    
-    if amr_df2.empty:
-        amr_txt = "No resistance gene detected!"
-    else:
-        amr_txt = amr_df2.to_string(index=False)
-    
-    if p_df2.empty:
-        plasmid_txt = "No plasmid is found!"
-    else:
-        plasmid_txt = p_df2.to_string(index=False)
-    
-    # write txt file
-    with open(samplename + "_txt_report.txt", "w") as f:
-        f.write(title + "\n\n")
-        # Sample Information
-        f.write(stitle1 + "\n")
-        f.write("Report date: " +  today.strftime("%B %d, %Y") + "\n")
-        f.write("Laboratory ID: " + samplename + "\n\n")
-        # Microorganisms
-        f.write(stitle2 + "\n")
-        f.write(b_df2.to_string(index=False) + "\n\n")
-        # MLST
-        f.write(stitle3 + "\n")
-        f.write(mlst_txt + "\n\n")
-        # AMR
-        f.write(stitle4 + "\n")
-        f.write(amr_txt + "\n\n")
-        # Plasmid
-        f.write(stitle5 + "\n")
-        f.write(plasmid_txt + "\n")   
-     
-
-    # HTML #
-    html = f'''
-    <!DOCTYPE html>
-    <html lang="en-us">
-    <head>
-        <meta charset="UTF-8">
-        <title>{page_title}</title>
-        <style>
-        h1 {{
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 1.5em;
-          text-align: center;
-          color: #1e5c85
-        }}
-
-        h2 {{
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 1em;
-          margin-top: 1em;
-          margin-bottom: 0.4em;
-          text-align: left;
-          color: #D6672E 
-        }}
-
-        table {{
-          border-collapse: collapse;
-          border: none;
-        }}
+        print("No mash result. Clia report won't be created!")
+        # create empty dataframe
+        mash = pd.DataFrame(columns=['Microorganism', 'Identity(%)'])
         
-         th, td {{
-          text-align: left;
-          padding-left: 5px;
-          padding-right: 5px;
-          padding-top: 1px;
-          padding-bottom: 1px;
-          border: none;
-        }}
-        
-        table.dataframe tr:nth-child(even) {{background-color: #f2f2f2;}}
-        
-        table.dataframe th {{
-          background-color: #1e5c85;
-          color: white;
-        
-        }}
-        footer {{
-			    font-size: 0.8em
-		    }}      
-        </style>
-    </head>
-    <body>
-    <header>
-      <h1>{title}</h1>
-    </header>
-    <hr>
-    <article>
-    <h2>{stitle1}</h2>
-     <table>
-      <tr>
-       <td>Report date:</td>
-       <td>{today}</td>
-      </tr>
-      <tr>
-       <td>Laboratory ID:</td>
-       <td>{samplename}</td>
-      </tr>
-     </table>
-    <h2>{stitle2}</h2>
-    {b_df2.to_html(index=False, justify="left")}
-    <h2>{stitle3}</h2>
-    {mlst_df.to_html(index=False, justify="left")}
-    <h2>{stitle4}</h2>
-    {amr_df2.to_html(index=False, justify="left")}
-    <h2>{stitle5}</h2>
-    {p_df2.to_html(index=False, justify="left")}
-    <p></p>
-    </article>
-    <hr>
-    <footer>
-      <p><i>This report is created by <a href="https://github.com/Kincekara/C-BIRD">{version}</a> bioinformatics pipeline.</br>
-      {footer_note}</i></p>
-    </footer>
-  </body>
-</html>
-'''
-    # write html file
-    with open(samplename + "_html_report.html", "w") as f:
-        f.write(html)               
+    # write summary report
+    epi_report(samplename, bracken, mlst, amr, stress, virulence, plasmid, blast, mash, version)       
         
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Summary report generating script for C-BIRD workflow')
+
+    parser.add_argument('-s', '--samplename', type=str, help='Sample name', required=True)
+    parser.add_argument('-t', '--taxon_report', type=str, help='Bracken report file', required=True)
+    parser.add_argument('-st', '--mlst_report', type=str, help='MLST report file', required=True)
+    parser.add_argument('-a', '--amr_report',  type=str, help='AMRFinderPlus report file', required=True)
+    parser.add_argument('-p', '--plasmid_report', type=str, help='PlasmidFinder report file', required=True)
+    parser.add_argument('-m', '--mash_report',  type=str, help='MASH report file')
+    parser.add_argument('-c', '--cbird_version',  type=str, help='C-BIRD version', required=True)
+    parser.add_argument('-f', '--footer_note',  type=str, help='Disclaimer text', default="")
+    parser.add_argument('-b', '--blast_report', type=str, help='Blast report file')
+
+    args = parser.parse_args()
+
+    main(args)
